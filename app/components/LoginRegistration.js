774d80bf5a3b7dc00b00859f732568586f690e89
MatthewKarpenko <mtw.karp@gmail.com>
2019-06-13 22:33:09 +0200

    finished validation

--- app/components/LoginRegistration.js
+++ app/components/LoginRegistration.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link,NavLink} from 'react-router-dom';
-import Registration from './Registration';
-import SignUp from './SignUp';
-
+import SignUpForm from './SignUpForm';
+import SignInForm from './SignInForm';
+import WindriderImg from '../images/head3.png'
 
 class LoginRegistration extends React.Component {
     render(){
         return(
-            <Router>
-                <div className="LogOrRegistration">
-                <NavLink  activeClassName="activeForm" to='/signIn'>Войти</NavLink>
-                <NavLink  activeClassName="activeForm" to='/Registration'>Регистрация</NavLink>
-                </div>
-                <Route path="/signIn" component={SignUp} />
-                <Route path="/Registration" component ={Registration} />
-           </Router>
+            <Router basename="/LogInOrRegistration#/">
+            <div className="App">
+              <div className="App__Aside">
+                  <img src={WindriderImg} alt="windriderlogo"></img>
+                  <p>"Join us"</p>
+              </div>
+              <div className="App__Form">
+                <div className="PageSwitcher">
+                    <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+                    <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+                  </div>
+    
+                  <div className="FormTitle">
+                      <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+                  </div>
+    
+                  <Route exact path="/" component={SignUpForm}>
+                  </Route>
+                  <Route path="/sign-in" component={SignInForm}>
+                  </Route>
+              </div>
+    
+            </div>
+          </Router>
         )
     }
 }
